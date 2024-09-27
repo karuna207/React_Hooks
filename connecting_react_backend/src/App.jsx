073bb7105eb65822854b0b7595478c1cdb0962ce
karuna207 <karunanidhiayyamperumal@gmail.com>
2024-09-27 21:37:54 +0530

    commiting connecting_react folder

--- connecting_react_backend/src/App.jsx
+++ connecting_react_backend/src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
+import { useState } from 'react' 
+import { useForm } from "react-hook-form"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { 
+  const {
+    register,
+    handleSubmit,
+    watch, 
+    setError,
+    formState: { errors ,isSubmitting},
+  } = useForm()
+ 
+
+  
+  const delay=async(d)=>{ 
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve()
+      },d*1000)
+
+    })
+    
+  }
+  const onSubmit = async (data) =>{ 
+      // await delay(4); 
+      let r = await fetch("http://localhost:3000/"); 
+      let res=await r.json();
+      console.log(data,res); 
+      
+    }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    {isSubmitting && <div>Loading...</div>}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("username",{required:true,minLength:{value:8,message:"Username is too short"}})} placeholder='Username' />     
+      {errors.username && <div>{errors.username.message}</div>}
+      <br/>
+      <input {...register("password", { required: true ,minLength:{value:6,message:"Password is too short"},maxLength:{value:12,message:"Password is too long"}})} placeholder="Password" /> 
+      <br/>
+      
+      <input disabled={isSubmitting} type="submit" /> 
+      {errors.myforms && errors.myforms.message}
+    </form>
+  
     </>
   )
 }
